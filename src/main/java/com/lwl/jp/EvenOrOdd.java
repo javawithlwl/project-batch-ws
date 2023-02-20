d25f89e16d3e71027e1b59463c1cf8b9339bfeab
javawithlwl <119342072+javawithlwl@users.noreply.github.com>
2023-02-20 22:23:13 +0530

    Update EvenOrOdd.java
    Changed ternary operator to if else

--- src/main/java/com/lwl/jp/EvenOrOdd.java
+++ src/main/java/com/lwl/jp/EvenOrOdd.java
@@ -5,7 +5,11 @@ public class EvenOrOdd {
   public static void main(String[] args) {
 
     int num = 19;
-    System.out.println(num % 2 == 0 ? "Even" : "Odd");
+    if(num % 2 == 0){
+      System.out.println(num+" is Even");
+    }else{
+      System.out.println(num+" is Odd");
+    }   
 
   }
 
