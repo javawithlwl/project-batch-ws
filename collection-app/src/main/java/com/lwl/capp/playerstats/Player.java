79b4a4a621f132ff7917e1d0fbf4b9a3ab105b4b
nareshavula2424 <nareshavula2424@gmail.com>
2023-02-23 16:03:51 +0530

    Adding all the implemented methods except dto classes

--- collection-app/src/main/java/com/lwl/capp/playerstats/Player.java
+++ collection-app/src/main/java/com/lwl/capp/playerstats/Player.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Player {
   private String name;
   private String role;
--- collection-app/src/main/java/com/lwl/capp/playerstats/PlayerServiceImpl.java
+++ collection-app/src/main/java/com/lwl/capp/playerstats/PlayerServiceImpl.java
@@ -1,14 +1,17 @@
 package com.lwl.capp.playerstats;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PlayerServiceImpl implements  PlayerService{
 
   private List<Player> players;
+  private Map<String, List<Player>> map;
 
   public PlayerServiceImpl(){
+
       players = CsvReaderUtil.loadDataFromCsv();
+      map = players.stream().collect(Collectors.groupingBy(Player::getTeam));
   }
 
   @Override
@@ -20,17 +23,26 @@ public class PlayerServiceImpl implements  PlayerService{
 
   @Override
   public List<String> getTeamNames() {
-    return null;
+    List<String> list = new ArrayList<>();
+    Set<String> teamNames = players.stream().map(ele -> ele.getTeam()).collect(Collectors.toSet());
+   list.addAll(teamNames);
+
+
+    return list;
   }
 
   @Override
   public List<Player> getPlayerByTeam(String team) {
-    return null;
+
+     List<Player> player= players.stream().filter(ele->ele.getTeam().equalsIgnoreCase(team)).collect(Collectors.toList());
+    return player;
   }
 
   @Override
   public List<Player> getPlayerByRole(String role) {
-    return null;
+       List<Player> player = players.stream().filter(ele->ele.getRole().equalsIgnoreCase(role)).collect(Collectors.toList());
+
+    return player;
   }
 
   @Override
@@ -39,23 +51,81 @@ public class PlayerServiceImpl implements  PlayerService{
   }
 
   @Override
-  public List<Player> getMaxPaidPlayers() {
-    return null;
+  public List<Player> getMaxPaidPlayers()
+  {
+    double maxAmount = getMaxAmount();
+     List<Player> player = players.stream().filter(ele->ele.getAmount()==maxAmount).collect(Collectors.toList());
+    return player;
+  }
+
+  private double getMaxAmount() {
+    double maxAmount = players.get(0).getAmount();
+
+    for(int i = 1;i<players.size();i++) {
+      if(players.get(i).getAmount()>maxAmount) {
+        maxAmount = players.get(i).getAmount();
+      }
+    }
+    return maxAmount;
+
+
   }
 
   @Override
   public List<Player> getMaxPaidPlayers(String team) {
-    return null;
+    List<Player> players = map.get(team);
+      List<Player> playersByTeam = players.stream().filter(ele->ele.getTeam().equalsIgnoreCase(team)).collect(Collectors.toList());
+      double maxAmount = getMaxAmountByTeam(playersByTeam);
+       List<Player> maxPaidPlayerByTeam = playersByTeam.stream().filter(ele->ele.getAmount()==maxAmount).collect(Collectors.toList());
+
+    return maxPaidPlayerByTeam;
+  }
+
+  private double getMaxAmountByTeam(List<Player> playersByTeam) {
+    double max = playersByTeam.get(0).getAmount();
+
+    for(Player player:playersByTeam){
+      if(player.getAmount()>max){
+        max = player.getAmount();
+      }
+
+    }
+    return max;
+
+
   }
 
   @Override
   public List<Player> getMaxPaidPlayers(String team, String role) {
-    return null;
+
+    List<Player> players = map.get(team);
+     List<Player> playersByRole = players.stream().filter(ele->ele.getRole().equalsIgnoreCase(role)).collect(Collectors.toList());
+    double maxAmount = getMaxAmountByTeamAndRole(playersByRole);
+      List<Player> maxPaidPlayers =  playersByRole.stream().filter(ele->ele.getAmount()==maxAmount).collect(Collectors.toList());
+
+
+
+
+    return maxPaidPlayers;
+  }
+  private double getMaxAmountByTeamAndRole(List<Player> playersByRole) {
+    double max = playersByRole.get(0).getAmount();
+    for (Player player : playersByRole) {
+      double amount = player.getAmount();
+      if (amount > max) {
+        max = amount;
+      }
+    }
+    return max;
   }
 
   @Override
   public List<String> getCountryNames() {
-    return null;
+    List<String> list = new ArrayList<>();
+    Set<String> country = players.stream().map(ele->ele.getCountry()).collect(Collectors.toSet());
+     list.addAll(country);
+
+    return list;
   }
 
   @Override
