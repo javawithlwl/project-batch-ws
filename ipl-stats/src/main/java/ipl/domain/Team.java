b750ac12d2949894d52febbc79c0b73da643cdd1
LakshmanaRao <ldevadhar@gmail.com>
2023-02-24 12:36:27 +0530

    Updates added for ipl-sttrts

---
+++ ipl-stats/src/main/java/ipl/domain/Team.java
@@ -0,0 +1,19 @@
+package ipl.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+
+public class Team {
+    public String Tname;
+    public String team;
+    public String captain;
+
+}
+
