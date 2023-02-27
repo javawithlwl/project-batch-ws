987153e923072a8235c2aa94e3e1dc922336f086
muralikrishnamuppa <125910866+muralikrishnamuppa@users.noreply.github.com>
2023-02-27 09:40:34 +0530

    OKK

--- collection-app/src/main/java/com/lwl/capp/json/IplStatManager.java
+++ collection-app/src/main/java/com/lwl/capp/json/IplStatManager.java
@@ -1,4 +1,54 @@
 package com.lwl.capp.json;
 
-public class IplStatManager {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import org.apache.commons.collections4.functors.ForClosure;
+
+import com.lwl.capp.playerstats.CsvReaderUtil;
+import com.lwl.capp.playerstats.Player;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@data
+class Player1{
+	private String name;
+	private String country;
+	private double amount;
 }
+
+class Role{
+	private String role;
+	private List<Player1> players1;
+}
+
+class Teams{
+	private String team;
+	private List<Role> roles;
+}
+
+class JsonEx{
+	private Teams t;
+}
+
+public class IplStatManager {
+	
+	public static void main(String[] args) {
+	List<Player> players = CsvReaderUtil.loadDataFromCsv();
+	Set<String> teams1 = players.stream().map(p->p.getTeam()).collect(Collectors.toSet());
+	Map<String,List<Player1>> teams11 = new HashMap<String, List<Player1>>();
+	//for (String  : teams1) {
+		
+	}
+	
+	
+	}
+		
+
